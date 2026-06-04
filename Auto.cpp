fdc0f22fe2e3b87ccfa9031a7a07547574e08721
Eze-R-Miquelarena <ezerosalesm@gmail.com>
2026-06-04 00:44:45 -0300

    Se agrega clase Auto

---
+++ Auto.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+ return 0;
+}#include "Auto.h"
+
+Auto::Auto()
+{
+  //ctor
+}
+
+Auto::~Auto()
+{
+  //dtor
+}
---
+++ Auto.h
@@ -0,0 +1,16 @@
+#ifndef AUTO_H
+#define AUTO_H
+
+
+class Auto
+{
+  public:
+    Auto();
+    virtual ~Auto();
+
+  protected:
+
+  private:
+};
+
+#endif // AUTO_H
